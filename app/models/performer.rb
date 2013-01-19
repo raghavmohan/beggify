bdbb511b9ec3951403405ee47d654f1476a6d041
Raghav Mohan <raghav.mohan@gmail.com>
2013-01-18 22:34:10 -0600

    merge

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,12 +1,8 @@
 class Performer < ActiveRecord::Base
-<<<<<<< HEAD
-   attr_accessible :first_name,:last_name, :money_ytd, :name, :picture_url, :street_name, :longitude, :latitude
-=======
   attr_accessible :venmo_id, :first_name, :last_latitude, :last_longitude, :last_name, :money_ytd, :name, :picture_url, :street_name
->>>>>>> 09986c82cb0e17d77aaff5cb2392703619d702ee
-   has_many :Performance
+  has_many :Performance
 
-   geocoded_by :address
-   after_validation :geocode, :if => :address_changed?
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
 
 end
