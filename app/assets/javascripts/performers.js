7278dc68c6c86331c61d39f07423874507e6c5aa
= <=>
2013-01-20 05:00:21 +0000

    bug fix

---
+++ app/assets/javascripts/performers.js
@@ -0,0 +1,44 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.mobile
+//= require_tree .
+
+function getLocation()
+{
+  if (navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(getPerformers);
+  }
+  else{console.log("Geolocation is not supported by this browser.");}
+}
+
+if (window.location.pathname == '/')
+	getLocation();
+
+
+function getPerformers(position)
+{
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+	//window.location.replace("performers/?lat="+latitude+"&long="+longitude);	
+	$.get("nearby", { latitude: latitude, longitude: longitude },
+  	function(data){
+  		console.log(data);
+  	}
+	);
+	
+}
+
